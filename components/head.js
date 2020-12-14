d1698a21ecc6b636fdd309b8d2c1f9b669dc5ecf
zcaiobs <zcaiobernardodasilva@gmail.com>
2020-12-14 01:25:25 -0300

    Fix bug

--- components/head.js
+++ components/head.js
@@ -5,7 +5,7 @@ export default function HeadImport() {
     <Head>
       <title>Suelier - Atelier de Costura</title>
       <link rel="shortcut icon" href="/favicon.svg" />
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
@@ -19,7 +19,7 @@ export default function HeadImport() {
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
         integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       />
       <link
         href="https://fonts.googleapis.com/css2?family=Cantarell&family=Montserrat&display=swap"
@@ -33,12 +33,12 @@ export default function HeadImport() {
       <script
         src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
         integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></script>
       <script
         src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-        crossorigin="anonymous"
+        crossOrigin="anonymous"
       ></script>
     </Head>
   );
